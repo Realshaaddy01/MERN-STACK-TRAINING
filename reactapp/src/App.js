02d224ae3ecf183833d9e7280b7ea0417c1d34c3
realshaaddy01 <realshaddy01@gmail.com>
2024-07-07 18:23:08 +0545

    main

---
+++ reactapp/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ClassComponent from "./classComponent";
+import Counter from "./counter";
+
+function App() {
+  return (
+   <>
+ <ClassComponent />
+ <Counter/>
+   </>
+  );
+}
+
+export default App;
